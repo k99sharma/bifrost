205e52985e5c48ff0679aa45575b94d2076f6815
Kalash Sharma <kalash.strt@gmail.com>
2023-02-04 22:09:23 +0530

    add: illustrations are changed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,10 @@ export default function Navbar() {
   return (
     <div className="navbar px-5 py-5">
       <Link to="/">
-        <Banner height={40} width={40} />
+        <div className="navbar-logo flex items-center">
+          <Banner height={40} width={40} />
+          <div className="text-white mx-2">Bifrost</div>
+        </div>
       </Link>
     </div>
   );
